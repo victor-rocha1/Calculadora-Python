1f48287f01ffdf95d6ab332bdf83449cbdd26325
Victor Rocha <145049490+victorti131@users.noreply.github.com>
2023-09-28 10:10:48 -0300

    Atualização da tela inicial
    Atualização dos pixels da calculadora e exibição do resultado final das operações.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import flet as ft
+import flet as ft   #Biblioteca usada
 
+#Página principal
 def main(page: ft.Page):
     page.bgcolor = '#000'
+    page.window_resizable = False
+    page.window_width = 300
+    page.window_height = 450
+    page.title = 'Calculadora'
+    page.window_always_on_top = True
+    
+    #Resultado das operações
+    resultado = ft.Text(value="0", color='FFFFFF', size=20)
+    
+    display = ft.Row(
+        width=300,
+        controls=[resultado],
+        alignment='end'
+    )
+    page.add(display)
+
+
+
+
 
 ft.app(target = main)
